2eb52ba12497b7bc0f91838550a7ae63eefbce0e
andrew wams <andrew.wams@gmail.com>
2020-03-15 23:58:55 +0100

    removed font-styles

--- lib/styles.dart
+++ lib/styles.dart
@@ -2,35 +2,30 @@ import 'package:flutter/material.dart';
 
 const soft_grey = Color.fromRGBO(52, 58, 64, 0.6);
 const appBarText = TextStyle(
-  fontFamily: 'Nunito',
   fontWeight: FontWeight.w700,
   color: Colors.white,
   fontSize: 25,
 );
 
 const cartText = TextStyle(
-  fontFamily: 'Nunito',
   color: Colors.white,
   fontSize: 20,
 );
 
 const h2 = TextStyle(
   color: soft_grey,
-  fontFamily: 'Nunito',
   fontSize: 18.0,
   fontWeight: FontWeight.w600,
   letterSpacing: 1.2,
 );
 const h3 = TextStyle(
   color: Colors.blueAccent,
-  fontFamily: 'Nunito',
   fontSize: 20.0,
   fontWeight: FontWeight.bold,
 );
 
 const p = TextStyle(
   color: soft_grey,
-  fontFamily: 'Nunito',
   fontSize: 16.0,
   fontWeight: FontWeight.w600,
 );
